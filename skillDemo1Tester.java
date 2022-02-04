b33d620f59936e4e0a1d01d99cd8b1351bf6adda
noahdj37 <noahdj37@gmail.com>
2022-02-03 22:54:32 -0800

    Add libs and failing test
    Added libs for JUnit testing, added failing multiply test

---
+++ skillDemo1Tester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemo1Tester {
+    @Test 
+    public void testMultiply(){
+        assertEquals(skillDemo1.multiply(3, 4), 55555);
+    }
+}
